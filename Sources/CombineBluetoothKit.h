14c9d80bdcde49a9def8a5ab086332bb8e4ebac4
Rafał Małczyński <rafal.malczynski@gmail.com>
2020-03-05 21:19:20 +0100

    Add initially setup project

---
+++ Sources/CombineBluetoothKit.h
@@ -0,0 +1,19 @@
+//
+//  CombineBluetoothKit.h
+//  CombineBluetoothKit
+//
+//  Created by Rafał Małczyński on 05/03/2020.
+//  Copyright © 2020 Rafał Małczyński. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CombineBluetoothKit.
+FOUNDATION_EXPORT double CombineBluetoothKitVersionNumber;
+
+//! Project version string for CombineBluetoothKit.
+FOUNDATION_EXPORT const unsigned char CombineBluetoothKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CombineBluetoothKit/PublicHeader.h>
+
+
